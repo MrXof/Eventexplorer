74546fd76b6bc5fe805c95d81dde9f828355ba0a
Danill Verulane <verualnedanill@gmail.com>
2024-04-17 11:52:42 +0300

    updata model

--- EventExplorer/API/Model.swift
+++ EventExplorer/API/Model.swift
@@ -8,23 +8,24 @@
 import Foundation
 
 // MARK: - PinTable
-struct PinTable: Codable {
+struct PinTable: Decodable {
   let records: [Position]
 }
 
 // MARK: - Record
-struct Position: Codable {
+struct Position: Decodable {
   let id, createdTime: String
   let fields: Place
 }
 
 // MARK: - Place
-struct Place: Codable {
-  let priceTier, date, address, name: String
-  let location, icon, category: String
+struct Place: Decodable {
+  let priceTier, date, address, location, name: String
+  let icon, category: String
+  let latitudeLocation , longitudeLocation: Double
   let usersGoing: Int
   let friendAvatars: [FriendAvatar]?
-  let friendsAreGoing: Bool?
+  var friendsAreGoing: Bool?
   
   enum CodingKeys: String, CodingKey {
     case priceTier = "price_tier"
@@ -33,10 +34,11 @@ struct Place: Codable {
     case friendAvatars = "friend_avatars"
     case friendsAreGoing = "friends_are_going"
   }
+  
 }
 
 // MARK: - FriendAvatar
-struct FriendAvatar: Codable {
+struct FriendAvatar: Decodable {
   let id: String
   let width, height: Int
   let url: String
@@ -44,5 +46,34 @@ struct FriendAvatar: Codable {
   let size: Int
   let type: String
 }
+
+extension Place {
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    location = try container.decode(String.self, forKey: .location)
+    priceTier = try container.decode(String.self, forKey: .priceTier)
+    date = try container.decode(String.self, forKey: .date)
+    address = try container.decode(String.self, forKey: .address)
+    name = try container.decode(String.self, forKey: .name)
+    icon = try container.decode(String.self, forKey: .icon)
+    category = try container.decode(String.self, forKey: .category)
+    usersGoing = try container.decode(Int.self, forKey: .usersGoing)
+    friendAvatars = try container.decodeIfPresent([FriendAvatar].self, forKey: .friendAvatars)
+  
+    if !container.contains(.friendsAreGoing) {
+        friendsAreGoing = false
+    }
+    
+    let locationArray = location.components(separatedBy: ", ")
+    latitudeLocation = Double(locationArray[0])!
+    longitudeLocation = Double(locationArray[1])!
+  }
+}
+
+                                   
+                                   
+
+
+
 
 
--- EventExplorer/ViewController/MapViewController.swift
+++ EventExplorer/ViewController/MapViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
@@ -13,22 +15,35 @@ class MapViewController: UIViewController, UICollectionViewDelegate, UICollectio
   @IBOutlet weak var collectionView: UICollectionView!
   @IBOutlet weak var headerView: UIView!
   @IBOutlet weak var blurHeaderView: UIVisualEffectView!
+  @IBOutlet weak var mapView: MKMapView!
   
   var currentFilter = ObjectStore.shared.arrayCategories[0]
+  var locationManager: CLLocationManager!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    locationManager = CLLocationManager()
+    locationManager.delegate = self
     
     fetch()
     createShadowView()
     createCornerRadius()
     collectionView.contentInset = .init(top: 0, left: 16, bottom: 0, right: 16)
     if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-        layout.scrollDirection = .horizontal
+      layout.scrollDirection = .horizontal
     }
     configureLocationButton()
     collectionView.dataSource = self
-    collectionView.delegate = self
+    
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+      self.locationManager.requestWhenInUseAuthorization()
+    }
   }
   
   func createCornerRadius() {
@@ -67,6 +82,8 @@ class MapViewController: UIViewController, UICollectionViewDelegate, UICollectio
     locationButton.configuration?.contentInsets.leading = 0
   }
   
+  //MARK: - CoolectionView
+  
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return ObjectStore.shared.arrayCategories.count
   }
@@ -101,5 +118,28 @@ class MapViewController: UIViewController, UICollectionViewDelegate, UICollectio
       }
     }
   }
+    
 }
 
+extension MapViewController: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    guard let location = locations.first else { return }
+    
+    let latitude = location.coordinate.latitude
+    let longitude = location.coordinate.longitude
+    
+    print("Latitude: \(latitude), Longitude: \(longitude)")
+    
+    let initialLocation = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    
+    let regionRadius: CLLocationDistance = 1000
+    let coordinateRegion = MKCoordinateRegion(center: initialLocation, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+    mapView.setRegion(coordinateRegion, animated: true)
+    
+    let annotation = MKPointAnnotation()
+    annotation.coordinate = initialLocation
+    annotation.title = "Мое текущее местоположение"
+    mapView.addAnnotation(annotation)
+  }
+  
+}
